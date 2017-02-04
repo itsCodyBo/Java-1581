53cf394a6dac4e27ca294b50ed833eeb429b6e3e
cody <codylbordelon@gmail.com>
2017-02-04 09:59:15 -0600

    Combat calculator pseduo code

---
+++ hw2/combatCalculator_pseudoCode.java
@@ -0,0 +1,61 @@
+//Top statement: determine the outcome of player's actions in combat against a monster
+
+//1. initalize monster and player variables
+	//1.1 Initialie monster name variable and set it to "Goblin"
+	//1.2 Initialize monster health variable and set to 100
+	//1.3 Initialize monster attack power variable and set to 15
+
+	//1.4 Initialize player health variable and set to 100
+	//1.5 Initialize player attack power variable and set it equal to 12
+	//1.6 Initialize player magic power variable and set it to 0
+
+
+//**Initialize loop control variable and set it to true
+
+
+//**While loop control variable is true (i.e. Monster is alive or player hasn't run)
+	//2. Print the starting monster and players stats
+		//2.1 Print the current values for the monster variables
+		//2.2 Print the current values for players variables
+
+
+
+
+
+
+
+	//3. Get player's input.
+		//3.1 Prompt the player for their action
+		//3.2 Print the current values for player variables
+
+
+
+
+
+
+
+
+	//4. Calculate and print result
+		//4.1 If the player chose option 1 (sword)
+			//4.1.1 calculate damage and update monster health using subtraction
+			//4.1.2 Print attack message
+		//4.2 Else if option 2 (cast spell)
+			//4.2.1 if player has 3 or more magic points
+				//4.2.1.1 Calculate damage and update monster health using division
+				//4.2.2.2 Print spell message
+			//4.2.2 Else
+				//4.2.2.1 Print error message	
+		//4.3 Else if option 3 (charge magic)
+			//4.3.1 Update player's magic points using addition
+			//4.3.2 Print charge magic message
+		//4.4 Else if option 4 (run away), calculate result and print option 4 message
+			//4.4.1 set loop variable to false
+			//4.4.2 Print run away message
+		//4.5 Else, print error message
+
+	
+
+	//5. If Monster health is 0 or less
+		//5.1 set loop control variable to false
+		//5.2 print victory message!!
+
